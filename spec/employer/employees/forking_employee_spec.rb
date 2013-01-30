072f385238a7cb84babc1e8442974d6ca1d1b93f
Mark Kremer <mark@without-brains.net>
2013-01-30 03:46:34 +0100

    Do not run ForkingEmployee specs for JRuby

--- spec/employer/employees/forking_employee_spec.rb
+++ spec/employer/employees/forking_employee_spec.rb
@@ -1,7 +1,7 @@
 require "employer/employees/forking_employee"
 require "support/shared_examples/employee"
 
-describe Employer::Employees::ForkingEmployee do
+describe Employer::Employees::ForkingEmployee, unless: RUBY_PLATFORM =~ /java/ do
   it_behaves_like "an employee"
   let(:employee) { Employer::Employees::ForkingEmployee.new }
   let(:job) { double("Job") }
