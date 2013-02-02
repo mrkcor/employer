34a66bc10bf6521dfb8b07eed7a3a4277350b6e7
Mark Kremer <mark@without-brains.net>
2013-02-02 07:16:37 +0100

    Removes useless tests

--- spec/employer/employees/forking_employee_spec.rb
+++ spec/employer/employees/forking_employee_spec.rb
@@ -3,17 +3,4 @@ require "support/shared_examples/employee"
 
 describe Employer::Employees::ForkingEmployee, unless: RUBY_PLATFORM =~ /java/ do
   it_behaves_like "an employee"
-  let(:employee) { Employer::Employees::ForkingEmployee.new }
-  let(:job) { double("Job") }
-
-  describe "#work" do
-    it "forks to perform job, and becomes busy" do
-      employee.free?.should be_true
-      employee.job.should be_nil
-      employee.should_receive(:fork)
-      employee.work(job)
-      employee.free?.should be_false
-      employee.job.should eq(job)
-    end
-  end
 end
--- spec/employer/employees/threading_employee_spec.rb
+++ spec/employer/employees/threading_employee_spec.rb
@@ -3,17 +3,4 @@ require "support/shared_examples/employee"
 
 describe Employer::Employees::ThreadingEmployee do
   it_behaves_like "an employee"
-  let(:employee) { Employer::Employees::ThreadingEmployee.new }
-  let(:job) { double("Job") }
-
-  describe "#work" do
-    it "starts new thread to perform job, and becomes busy" do
-      employee.free?.should be_true
-      employee.job.should be_nil
-      Thread.should_receive(:new)
-      employee.work(job)
-      employee.free?.should be_false
-      employee.job.should eq(job)
-    end
-  end
 end
